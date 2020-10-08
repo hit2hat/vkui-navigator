b5e83c115697404c01176949c737c5608eabef7a
Пронин Егор <mrzillagold@gmail.com>
2020-10-08 16:00:07 +1000

    Tabbar show param

--- src/Tabbar.jsx
+++ src/Tabbar.jsx
@@ -6,8 +6,9 @@ import { Epic, Tabbar, TabbarItem } from "@vkontakte/vkui";
  * @description Умный таббар
  * @param {string} id - Идентификатор, который передается Epic внутри компонента
  * @param {string} activeStory - Идентификатор начальной страницы (Page)
+ * @param {boolean} show=true - Показывать таббар
  */
-export default function NTabbar({ activeStory: activeTabbarStory, id, children, showModal, hideModal, goPage, isModalOpen, pageParams }) {
+export default function NTabbar({ activeStory: activeTabbarStory, id, children, showModal, hideModal, goPage, isModalOpen, pageParams, show = true }) {
 
 	const [{ activeStory }, setTabbar] = React.useReducer((state, updatedState) => {
 		return {
@@ -42,8 +43,10 @@ export default function NTabbar({ activeStory: activeTabbarStory, id, children, 
 		);
 	};
 
+	const tabbar = __buildBar(children);
+
 	return (
-		<Epic id={id} activeStory={activeStory} tabbar={__buildBar(children)}>
+		<Epic id={id} activeStory={activeStory} tabbar={show ? tabbar : null}>
 			{React.Children.map(children, (Child) =>
 				React.cloneElement(Child, {
 					...Child.props,
